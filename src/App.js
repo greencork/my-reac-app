e8cec1a27995f2f7aa02f14e6b660e66f49109b5
greencork <greencork@protonmail.com>
2022-03-31 00:08:19 +0900

    updates to App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import logo from './logo.svg'
 import './App.css';
 import { Authenticator } from '@aws-amplify/ui-react'
 import '@aws-amplify/ui-react/styles.css'
@@ -10,8 +9,7 @@ function App() {
       {({signOut}) => (
         <div className="App">
           <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <h1>Hello World</h1>
+            <h1>Hello World test</h1>
           </header>
           <button onClick={signOut}>Sign Out</button>
         </div>
